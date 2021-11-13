655ff67123ee6c13aed7e570b5ec05e21e0ca940
beatrizcunharo <beatrizcunharo@gmail.com>
2021-11-12 23:09:03 -0300

    feat: Leitura finalizada (ainda analisar a leitura por blocos)

--- trabalho-parte-1/Processamento.h
+++ trabalho-parte-1/Processamento.h
@@ -11,7 +11,15 @@
 #ifndef PROCESSAMENTO_H_INCLUDED
 #define PROCESSAMENTO_H_INCLUDED
 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+
 #include "Data.h"
+using std::stringstream;
+using std::string;
 using namespace std;
 
 class Processamento 
@@ -19,12 +27,87 @@ class Processamento
     public:
 
     // CONSTRUTOR
-    Processamento();
+    Processamento()
+    {
+        // CONSTRUTOR VAZIO
+    }
 
     // DESTRUTOR
-    ~Processamento();
+    ~Processamento()
+    {
+        // DESTRUTOR VAZIO
+    }
+
+    // LEITURA DO ARQUIVO EM CSV
+
+    void leituraArquivo(vector<Data> *dados, ifstream& arquivoEntrada)
+    {
+        Data dadosLidos;
+        string linha;
+
+        // RETIRANDO A PRIMEIRA LINHA POIS É APENAS OS NOMES DOS CAMPOS
+        getline(arquivoEntrada, linha, '\n');
+        linha.clear();
+
+        while(!arquivoEntrada.eof())
+        {         
+            // LENDO REVIEW_ID
+            getline(arquivoEntrada, linha, ',');
+            dadosLidos.setReviewId(linha);
+            linha.clear();
+
+            // LENDO REVIEW_TEXT
+            if(arquivoEntrada.peek()=='"')
+            {
+                arquivoEntrada.get();
+                getline(arquivoEntrada,linha,'"');
+                dadosLidos.setReviewText(linha);
+                linha.clear();
+            }
+            else 
+            {
+                getline(arquivoEntrada,linha,',');
+                dadosLidos.setReviewText(linha);
+                linha.clear();
+            }
+
+            // LENDO UPVOTES
+            getline(arquivoEntrada, linha,',');
+            dadosLidos.setUpvotes(atoi(linha.c_str()));
+            linha.clear();
+
+            // LENDO APP_VERSION
+            getline(arquivoEntrada, linha, ',');
+            dadosLidos.setAppVersion(linha);
+            linha.clear();
+
+            // LENDO POSTED_DATE
+            getline(arquivoEntrada, linha, '\n');
+            dadosLidos.setPostedDate(linha);
+            linha.clear();
+            
+            dados->push_back(dadosLidos);
+        }
+    }
     
-    private:
+    void escritaArquivo(vector<Data> dados)
+    {
+        fstream arquivoSaida("arquivos/tiktok_app_reviews.bin", ios::binary);
+
+        // VERIFICA SE O ARQUIVO ESTÁ ABERTO
+
+        if(arquivoSaida.is_open())
+        {
+            for(int i=0;i < dados.size(); i++) 
+            {
+                arquivoSaida.write(reinterpret_cast<const char*>(dados[i].getReviewId().c_str()), dados[i].getReviewId().length());
+                arquivoSaida.write(reinterpret_cast<const char*>(dados[i].getReviewText().c_str()), dados[i].getReviewText().length());
+                arquivoSaida.write(reinterpret_cast<const char*>(dados[i].getUpvotes()), sizeof(dados[i].getUpvotes()));
+                arquivoSaida.write(reinterpret_cast<const char*>(dados[i].getAppVersion().c_str()), dados[i].getAppVersion().length());
+                arquivoSaida.write(reinterpret_cast<const char*>(dados[i].getPostedDate().c_str()), dados[i].getPostedDate().length());
+            }
+        }
+    }
 
 };
 
--- trabalho-parte-1/main.cpp
+++ trabalho-parte-1/main.cpp
@@ -9,7 +9,16 @@
 */
 #include <fstream>
 #include <vector>
-
+#include<bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <chrono>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <random>
 
 #include "Data.h"
 #include "Processamento.h"
@@ -24,15 +33,48 @@ void prefacio()
     cout << "Daniel Ribeiro Lavra - 201735042" << endl;
 }
 
+void leitura()
+{
+    Processamento processamento;
+
+    ifstream arquivoEntrada("arquivos/tiktok_app_reviews.csv");
+
+    // CONFERE SE O ARQUIVO ESTÁ ABERTO
+
+    if(!arquivoEntrada.is_open())
+    {
+        cout << "Erro. Arquivo não pode ser aberto." << endl;
+        exit(1);
+    }
+
+    // VETOR QUE GUARDA OS REGISTROS
+
+    vector<Data> dados;
+
+    // LEITURA DO ARQUIVO .CSV - VERIFICAR IMPLEMENTAÇÃO COM CHAR
+
+    cout << "Lendo o arquivo..." << endl;
+    processamento.leituraArquivo(&dados, arquivoEntrada);
+    cout << "Arquivo Lido" << endl;
+    arquivoEntrada.close();
+
+    // ESCRITA DO ARQUIVO .BIN - ARRUMAR
+
+    /*cout << "Criando arquivo .bin" << endl;
+    processamento.escritaArquivo(dados);
+    cout << "Arquivo criado." << endl;*/
+
+}
+
 void processamento()
 {
     int op = 0;
     while(op != -1)
     {
         cout << "Escolha uma opcao: " << endl;
-        cout << "1 - Processamento dos dados;" << endl;
-        cout << "2 - Acessar Registro;" << endl;
-        cout << "3 - Funcao de teste;" << endl;
+        cout << "1 - Pre-processamento dos dados;" << endl;
+        cout << "1 - Acessar Registro;" << endl;
+        cout << "2 - Funcao de teste;" << endl;
         cout << "-1 - Sair" << endl;
         cout << "Opcao: ";
         cin >> op;
@@ -42,7 +84,7 @@ void processamento()
                 cout << "Saindo..." << endl;
                 exit(0);
             case 1:
-                // FAZER PROCESSAMENTO
+                // FAZER PRE PROCESSAMENTO DOS DADOS
                 exit(0);
             case 2: 
                 // FAZER ACESSO AO REGISTRO
@@ -58,6 +100,14 @@ void processamento()
 
 int main(int argc, char *argv[])
 {
+    // CONFERE SE HOUVE A ENTRADA DO ARQUIVO
+    /*if (argc != 2) 
+    {
+        prefacio();
+        cout << "ERROR: Expecting: ./<nomePrograma> <arquivo> " << endl;
+        return 0;
+    }*/
+    leitura();
     prefacio();
     processamento();
 }
